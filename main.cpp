52d4302d4ebfdd842908a723aab5d8b11baa6429
DR1PST4R <139138163+DR1PST4R@users.noreply.github.com>
2024-06-12 21:14:55 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(nullptr));
+	bool guessedNumber = false;
+	int guessCount = 0;
+	int ourGuess;
+	int computerNumber = rand() % 100 + 1;
+
+
+
+	while (!guessedNumber)
+	{
+		cout << "Enter your guess"<< endl;
+		cin >> ourGuess;
+
+		if (ourGuess > 100 || ourGuess < 1)
+		{
+			cout << "Guess for real this time" << endl;
+			guessCount++;
+			continue;
+		}
+		if (ourGuess == computerNumber)
+		{
+			cout << "Congratulations you guessed the real number in " << guessCount << " guesses! " << endl;
+			cout << "Thanks for playing" << endl;
+			guessedNumber = true;
+		}
+		else if (ourGuess > computerNumber)
+		{
+			cout << "Your guess is too high!" << endl;
+		}
+		else
+		{
+			cout << "Your guess is too low!" << endl;
+
+		}
+		guessCount++;
+
+	}
+
+
+
+	return 0;
+}
